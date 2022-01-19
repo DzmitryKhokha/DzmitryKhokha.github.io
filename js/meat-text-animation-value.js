af98ab3a4fbe995a877d1373d4960ca6d9a1d2a0
DzmitryKhokha <92515602+DzmitryKhokha@users.noreply.github.com>
2022-01-19 16:24:39 +0300

    Add files via upload

--- js/meat-text-animation-value.js
+++ js/meat-text-animation-value.js
@@ -2,17 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     const scrollValueItems = document.querySelectorAll('.scroll-item')
+    const addClass = document.querySelectorAll('.beef')
 
     const scrollAnimation = () => {
         let windowCenter = (window.innerHeight * 1.2) + window.scrollY;
         scrollValueItems.forEach(el => {
-            let scrollOffSet = el.offsetTop + (el.offsetHeight / 2);
+            let scrollOffSet = el.offsetTop + (el.offsetHeight / 3);
             if (windowCenter >= scrollOffSet) {
                 el.classList.add('animation-class');
             } else {
                 el.classList.remove('animation-class');
             }
         });
+
+        let windowQuarter = (window.innerHeight / 5) + window.scrollY;
+        addClass.forEach(el => {
+            let scrollOffSet = el.offsetTop + (el.offsetHeight*0.8);
+            if(windowQuarter >= scrollOffSet) {
+                el.classList.add('anim-class');
+            } else {
+                el.classList.remove('anim-class')
+            }
+        })
     };
 
 
